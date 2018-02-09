f7ede4c8dbf0dd185172dbbdc5d8d6adc0e0fdcd
Matthew Vogt <mvogt0000@gmail.com>
2018-02-09 11:41:12 -0500

    initial commit

---
+++ Meat Spin/Meat Spin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meat Spin
+//
+//  Created by Matthew Vogt on 2/9/18.
+//  Copyright © 2018 SWEN-356. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Meat Spin/Meat SpinTests/Meat_SpinTests.swift
@@ -0,0 +1,36 @@
+//
+//  Meat_SpinTests.swift
+//  Meat SpinTests
+//
+//  Created by Matthew Vogt on 2/9/18.
+//  Copyright © 2018 SWEN-356. All rights reserved.
+//
+
+import XCTest
+@testable import Meat_Spin
+
+class Meat_SpinTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
