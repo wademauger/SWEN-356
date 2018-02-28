cccaa033fdd472df3b3a5be2ac93da873964d007
Arsh <arsh2223@gmail.com>
2018-02-28 12:08:09 -0500

    Adding tables ui

---
+++ Meat Spin/Meat Spin WatchKit Extension/UpgradeInterfaceController.swift
@@ -0,0 +1,18 @@
+//
+//  UpgradeInterfaceController.swift
+//  Meat Spin WatchKit Extension
+//
+//  Created by Arsh Singh on 2/28/18.
+//  Copyright © 2018 SWEN-356. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class UpgradeInterfaceController: WKInterfaceController {
+
+    
+    @IBOutlet var upgradesTable: WKInterfaceTable!
+    
+}
